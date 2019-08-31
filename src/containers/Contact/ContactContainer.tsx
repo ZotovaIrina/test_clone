28ce57a2e046e5b19a190bdb6dec0ca4bd4d129b
irinazotova <zotovaiv87@gmail.com>
2019-08-31 15:49:04 -0400

    Minor changes

--- src/containers/Contact/ContactContainer.tsx
+++ src/containers/Contact/ContactContainer.tsx
@@ -1,10 +1,11 @@
 import ContactMe from './ContactMe';
 import {IStore} from '../../store/Store';
 import {connect} from 'react-redux';
-import {withRouter} from 'react-router';
 import {submitContactMeForm, updateContactMeForm} from '../../store/contactForm/contactFormDispatch';
 import getConfig, {AppConfigs} from '../../configs/getConfig';
 import IContactMe from '../../configs/formConfigs/contactMe';
+import React from 'react';
+import {IContactFormProps} from '../../components/contactMe/contactForm';
 
 const mapStateToProps = (state: IStore) => ({
   formData: state.contactForm.formData,
@@ -18,11 +19,23 @@ const mapDispatchToProps = () => ({
   onSubmit: submitContactMeForm
 });
 
-const ContactContainer = withRouter(
-  connect(
-    mapStateToProps,
-    mapDispatchToProps
-  )(ContactMe) as any
-);
+export class ContactContainer extends React.Component<IContactFormProps> {
 
-export default ContactContainer;
+  render(){
+    return (
+      <ContactMe
+        formData={this.props.formData}
+        formError={this.props.formError}
+        pageConfig={this.props.pageConfig}
+        config={this.props.config}
+        onChange={this.props.onChange}
+        onSubmit={this.props.onSubmit}
+      />
+    );
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ContactContainer);
